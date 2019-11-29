7c25d0259288a666c9aaf08a57e3ad63ec2eb46e
clagosuarez <clagosuarez@danielcastelao.org>
2019-11-29 13:38:28 +0100

    Cambio de nombre variables j,f a jota,efe

---
+++ src/factorial/Factorial.java
@@ -0,0 +1,38 @@
+
+package factorial;
+
+
+public class Factorial {
+
+
+    public static void main(String[] args) {
+
+
+    int jota;
+    int efe;
+
+  
+   jota = 8;    
+   
+
+    
+   int i;
+   if (jota == 0)
+efe = 1;
+   else
+   {
+efe = 1;
+for (i = jota; i >= 1; i--)
+{
+    efe = efe * i;
+}
+   }    
+  
+ 
+
+System.out.println(efe); 
+
+
+    }
+    
+}
