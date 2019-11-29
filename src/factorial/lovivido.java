80d7369af3c7765ab8b83844a3c3baa6cf362479
clagosuarez <clagosuarez@danielcastelao.org>
2019-11-29 13:47:06 +0100

    Cambio de nombres de variables de n,e,m,d,h,a a nombre,edad,meses,dias,horas,anhos

---
+++ src/factorial/lovivido.java
@@ -0,0 +1,42 @@
+/*
+ * Examen Refactorizacion
+ * 
+ */
+package factorial;
+
+import javax.swing.JOptionPane;
+
+public class lovivido {
+
+public static void main(String[] args) {
+String nombre;
+String edad;
+
+int meses;
+int dias;
+int horas;
+int anhos;
+
+nombre = JOptionPane.showInputDialog ("Escriba su nombre: ");
+edad = JOptionPane.showInputDialog ("Escriba su edad: ");
+ 
+anhos = Integer.parseInt (edad);
+ 
+meses = (anhos*12);
+dias = (anhos*365);
+horas = (dias*24);
+ 
+ JOptionPane.showMessageDialog(null,  "Meses: "  +meses,"Numero de meses vividos de " +nombre,JOptionPane.INFORMATION_MESSAGE);
+
+
+ JOptionPane.showMessageDialog(null, "Días: " +dias,"Numero de días vividos de "+nombre,JOptionPane.INFORMATION_MESSAGE);
+
+
+ JOptionPane.showMessageDialog(null, "Horas: " +horas,"Numero de horas vividos de "+nombre,JOptionPane.INFORMATION_MESSAGE);
+
+   System.exit (0);
+
+}
+   
+
+}
